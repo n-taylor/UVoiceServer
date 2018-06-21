edb0c41a9a2e8f154dcbcc0bce81273d664d9b88
n-taylor <natekeithtaylor@gmail.com>
2018-06-21 12:34:34 -0600

    Add missing pieces

---
+++ app/edw/controllers/edw.js
@@ -0,0 +1,18 @@
+var edw = require('../models/edwDao');
+
+exports.censusByUnit = function (req, res){
+    var unit = req.params.unit.toUpperCase();
+    edw.censusByUnit(req, res, unit);
+}
+
+exports.allUnitsCensus = function(req, res){
+    edw.allUnitsCensus(req, res);
+}
+
+exports.allProceduresCategories = function(req, res){
+    edw.allProceduresCategories(req, res);
+}
+
+exports.getProceduresBySearchParams = function(req, res){
+    edw.getProceduresBySearchParams(req, res);
+}
---
+++ app/edw/models/edwDao.js
@@ -0,0 +1,91 @@
+var constants = require('../../../config/constants');
+var request = require('request');
+
+exports.censusByUnit = function(req, res, unit){
+    var options = {
+        uri: constants.url_bed_census_unit + "/" + unit,
+        method: 'GET',
+        headers: req.headers,
+        rejectUnauthorized: false, // DELETE THIS!!!!        
+    }
+
+    request(options, function(err, response){
+        if (err){
+            res.send(err.message);
+        }
+        else{
+            res.headers = response.headers;
+            res.send(response.body);
+        }
+    })
+}
+
+exports.allUnitsCensus = function(req, res){
+    var options = {
+        uri: constants.url_bed_census_all,
+        method: 'GET',
+        headers: req.headers,
+        rejectUnauthorized: false, // DELETE THIS!!!!        
+    }
+
+    request(options, function(err, response){
+        if (err){
+            res.send(err.message);
+        }
+        else{
+            res.headers = response.headers;
+            res.send(response.body);
+        }
+    })
+}
+
+exports.allProceduresCategories = function(req, res){
+    var options = {
+        uri: constants.url_procedures_all_categories,
+        method: 'GET',
+        headers: req.headers,
+        rejectUnauthorized: false, // DELETE THIS!!!!        
+    }
+
+    request(options, function(err, response){
+        if (err){
+            res.send(err.message);
+        }
+        else{
+            res.headers = response.headers;
+            res.send(response.body);
+        }
+    });
+}
+
+exports.getProceduresBySearchParams = function(req, res){
+    var params = req.query;
+
+    var path = '';
+    if (params.category){
+        path += "/" + params.category;
+        if (params.subCategory){
+            path += "/" + params.subCategory;
+            if (params.extremity){
+                path += "/" + params.extremity;
+            }
+        }
+    }
+
+    var options = {
+        uri: constants.url_procedures_codes_by_hierarchy + path,
+        method: 'GET',
+        headers: req.headers,
+        rejectUnauthorized: false, // DELETE THIS!!!!
+    }
+
+    request(options, function(err, response){
+        if (err){
+            res.send
+        }
+        else {
+            res.headers = response.headers;
+            res.send(response.body);
+        }
+    });
+}
