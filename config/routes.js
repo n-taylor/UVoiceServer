cc4c3a6f1b136a3cacbf256f6a21382daa647141
Nathan Taylor <natekeithtaylor@gmail.com>
2018-06-26 14:01:20 -0600

    Replace a deleted line handling /onCall/getPagers

--- config/routes.js
+++ config/routes.js
@@ -20,16 +20,11 @@ module.exports = function (app) {
     app.get('/bedCensus', home.loggedIn, edw.allUnitsCensus);
     app.get('/bedCensus/all', home.loggedIn, edw.allUnitsCensus);
     app.get('/bedCensus/unit/:unit', home.loggedIn, edw.censusByUnit);
-<<<<<<< HEAD
-    app.post('/onCall/getMID', home.getMID);
-    app.post('/onCall/getNumbers', home.getNumbers);
-    app.post('/onCall/getPagers', home.getPagers);
-=======
 
     app.post('/onCall/getMID', home.loggedIn, home.getMID);
     app.post('/onCall/getNumbers', home.loggedIn, home.getNumbers);
     app.post('/onCall/getMID', home.loggedIn, home.getMID);
->>>>>>> master
+    app.post('/onCall/getPagers', home.loggedIn, home.getPagers);
 
     app.post('/login', home.login);
     app.get('/logout', home.loggedIn, home.logout);
